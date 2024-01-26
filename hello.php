8d63422b72da8c7bbae7798ff861fecdaee9eba0
Your Name <143082313+blessingHamk@users.noreply.github.com>
2024-01-26 11:03:31 +0200

    Initial commit

---
+++ hello.php
@@ -0,0 +1,17 @@
+echo "hello world";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Test</title>
+</head>
+<body>
+    
+<?php
+// this is printing hello world
+echo "<h1>hello world this is test</h1>";
+phpinfo();
+?>
+</body>
+</html>
